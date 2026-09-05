c8f03b537523ec0f1853e3e34b97807b157401d1
mikonova <mikk4business@gmail.com>
2026-09-05 14:43:34 +0300

    [CHORE] created project

---
+++ errdef/errors.go
@@ -0,0 +1,7 @@
+package errdef
+
+var (
+	ErrBase  string = "\033[0;31m[ERROR]\033[0;37m OpenGate: "
+	WarnBase string = "\033[0;33m[WARN]\033[0;37m OpenGate: "
+	InfoBase string = "\033[0;32m[INFO]\033[0;37m OpenGate: "
+)
